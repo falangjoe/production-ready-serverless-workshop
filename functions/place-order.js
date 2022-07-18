85accaeb29cbc76a5902f6b4f92b985da224ecdf
jsmith3 <jsmith3@1800contacts.com>
2022-07-18 14:06:14 -0600

    adding place order stuff

---
+++ functions/place-order.js
@@ -0,0 +1,36 @@
+const EventBridge = require('aws-sdk/clients/eventbridge')
+const eventBridge = new EventBridge()
+const chance = require('chance').Chance()
+
+const busName = process.env.bus_name
+
+module.exports.handler = async (event) => {
+  const restaurantName = JSON.parse(event.body).restaurantName
+
+  const orderId = chance.guid()
+  console.log(`placing order ID [${orderId}] to [${restaurantName}]`)
+
+  const entry = {
+    Source: 'big-mouth',
+    DetailType: 'order_placed',
+    Detail: JSON.stringify({
+      orderId,
+      restaurantName,
+    }),
+    EventBusName: busName
+  };
+
+
+  await eventBridge.putEvents({
+    Entries: [entry]
+  }).promise()
+
+  console.log(`published 'order_placed' event into EventBridge`)
+
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify({ orderId })
+  }
+
+  return response
+}
